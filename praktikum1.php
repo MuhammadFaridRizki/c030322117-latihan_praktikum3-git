6ffd1155bd8e749c63c60610453b4cb1627c9318
MuhammadFaridRizki <144120315+MuhammadFaridRizki@users.noreply.github.com>
2023-09-22 16:00:39 +0700

    praktikum 3

---
+++ praktikum1.php
@@ -0,0 +1,18 @@
+ <form action="hasil.php" method="post">
+
+
+<h1> Formulir <h1/>
+//Memberi tampilan awal dengan nama formulir dengan ukuran <h1>, itu artinya tulisan akan di tampilkan tebal (besar)
+  <tr>
+   <td> Jumlah Lembar Fotocopy </td>
+//Menampilkan tulisan jumlah lembar fotocopy
+
+   <td><input type="text" name="jumlah"></d>
+  </tr>
+//membuat form inputan  dengan tipe inputan yaitu text 
+
+  <tr>
+<input type="submit" name="hitung" value="Hitung"></td>
+  </tr>
+//membuat tombol button submit, dengan type submit, dan dengan nama tombol button yaitu hitung
+ </form>
